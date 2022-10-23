4ddd613b809e80f3429e728cf37b65c7c3bff20a
Siddhant Singh <siddhantk.singh.phe14@itbhu.ac.in>
2022-10-23 16:37:18 +0530

    Week 8 problem 3

--- Week-8/Problem3/Problem3.js
+++ Week-8/Problem3/Problem3.js
@@ -1,5 +1,3 @@
-let arr = [3, 9, 20, null, null, 15, 7]
-
 class Node {
     constructor(data){
         this.left = null
@@ -11,38 +9,56 @@ class Node {
 class Tree {
     constructor(root){
         this.root = root
-        this.arr = []
+        this.nodes = []
     }
 
-    printPreorder(root){
+    printPreOrder(root){
         if(root==null){
             return
         }
-        this.arr.push(root.data)
-        this.printPreorder(root.left)
-        this.printPreorder(root.right)
+        console.log(root.data)
+        this.printPreOrder(root.left)
+        this.printPreOrder(root.right)
     }
 }
 
-
-let array = []
-
-for(let i=0;i<arr.length;i++){
-    let left = 2*i+1
-    let right = 2*i+2
-    if(left<=arr.length-1 && right <= arr.length-1){
-        let node = new Node(arr[i])
-        node.left = new Node(arr[left])
-        node.right = new Node(arr[right])
-        array.push(node)
+function insertTreeNode(i, arr){
+    let curr = null
+    if(i<arr.length){
+        curr = new Node(arr[i])
+        curr.left = insertTreeNode(2*i+1, arr)
+        curr.right = insertTreeNode(2*i+2, arr)
     }
+    return curr
 }
 
+function createTree(arr){
+    return insertTreeNode(0, arr)
+}
 
-console.log(array)
+let arr = [3, 9, 20, null, null, 15, 7]
 
-let tree = new Tree(array[0])
+let root = createTree(arr)
 
-tree.printPreorder()
+// pre order traversal
+let tree = new Tree(root)
+tree.printPreOrder(tree.root)
+
+
 // level order traversal
-console.log(arr)
+let queue = []
+let outarr = []
+queue.push(root)
+
+while (queue.length){
+    let temp = queue.shift()
+    outarr.push(temp)
+    if(temp.left!=null){
+        queue.push(temp.left)
+    }
+    if(temp.right!=null){
+        queue.push(temp.right)
+    }
+}
+// level order traversal
+console.log(outarr)
